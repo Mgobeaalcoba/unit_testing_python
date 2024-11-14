c970af099d6fadd28255cee58755e27023398d61
Mariano Daniel Gobea Alcoba <mariano.gobea@mercadolibre.com>
2024-11-14 12:56:56 -0300

    :sparkles: Add first class and unittest

---
+++ tests/tests_calculator.py
@@ -0,0 +1,25 @@
+import unittest
+from unit_testing_python.calculator import Calculator
+
+
+class CalculatorTests(unittest.TestCase):
+    def test_sum(self):
+        self.assertEqual(Calculator.add(1, 1), 2)
+        self.assertEqual(Calculator.add(1, -1), 0)
+
+    def test_subtract(self):
+        self.assertEqual(Calculator.subtract(1, 1), 0)
+        self.assertEqual(Calculator.subtract(1, -1), 2)
+
+    def test_multiply(self):
+        self.assertEqual(Calculator.multiply(1, 1), 1)
+        self.assertEqual(Calculator.multiply(1, -1), -1)
+
+    def test_divide(self):
+        self.assertEqual(Calculator.divide(1, 1), 1)
+        self.assertEqual(Calculator.divide(1, -1), -1)
+        self.assertEqual(Calculator.divide(1, 0), 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ unit_testing_python/calculator.py
@@ -0,0 +1,24 @@
+# Calculator class
+
+class Calculator:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def subtract(a, b):
+        return a - b
+
+    @staticmethod
+    def multiply(a, b):
+        return a * b
+
+    @staticmethod
+    def divide(a, b):
+        if b != 0:
+            return a / b
+        else:
+            return 0
