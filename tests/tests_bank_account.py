2dec7b54c94de3935c74c4284914bebd80c5198f
Mariano Daniel Gobea Alcoba <mariano.gobea@mercadolibre.com>
2024-11-15 08:36:04 -0300

    :sparkles: Add new test method: tearDown

--- tests/tests_bank_account.py
+++ tests/tests_bank_account.py
@@ -1,4 +1,6 @@
 import unittest
+import os
+from unittest import mock
 from unit_testing_python.bank_account import BankAccount
 
 
@@ -9,7 +11,10 @@ class BackAccountTests(unittest.TestCase):
         This method is called before each test.
         It is used to set up the test environment.
         """
-        self.account = BankAccount(2000)
+        self.account = BankAccount(2000, 'test_log.txt')
+        self.other_account = BankAccount(1000, 'test_log2.txt')
+        with mock.patch('builtins.input', return_value='no'):
+            self.account_with_no_log = BankAccount(2000)
 
     def test_deposit(self):
         new_balance = self.account.deposit(1000)
@@ -32,17 +37,76 @@ class BackAccountTests(unittest.TestCase):
             self.account.withdraw(5000)
 
     def test_transfer(self):
-        other_account = BankAccount(1000)
-        new_balance = self.account.transfer(500, other_account)
+        new_balance = self.account.transfer(500, self.other_account)
         self.assertEqual(new_balance, 1500)
-        self.assertEqual(other_account.get_balance(), 1500)
+        self.assertEqual(self.other_account.get_balance(), 1500)
 
     def test_negative_transfer(self):
-        other_account = BankAccount(1000)
         with self.assertRaises(ValueError):
-            self.account.transfer(-500, other_account)
+            self.account.transfer(-500, self.other_account)
 
     def test_over_transfer(self):
-        other_account = BankAccount(1000)
         with self.assertRaises(ValueError):
-            self.account.transfer(5000, other_account)
+            self.account.transfer(5000, self.other_account)
+
+    def test_deposit_without_log_file(self):
+        with mock.patch('builtins.input', return_value='no'):
+            new_balance = self.account_with_no_log.deposit(1000)
+        self.assertEqual(new_balance, 3000)
+
+    def test_deposit_in_account_without_log_file_and_creating_the_same(self):
+        """
+        This test case tests the deposit method in an account without a log file.
+        It also tests creating a log file after the deposit.
+
+        The deposit method should return the new balance and a log file should be created.
+        """
+        with mock.patch('builtins.input', side_effect=['yes', 'test_log.txt']):
+            new_balance = self.account_with_no_log.deposit(1000)
+        self.assertEqual(new_balance, 3000)
+        self.assertTrue(os.path.exists('test_log.txt'))
+
+    def test_log_file_is_created_and_messages_are_logged(self):
+        """
+        This test case tests if the log file is created and messages are logged in the file.
+        Also, it tests the number and the content of messages logged in the file.
+        This is very important because it ensures that the log file is created and messages are logged correctly.
+        Don't add coverage to the test results, because we are not testing the coverage of the code
+        but the functionality of the code.
+
+        The log file should be created and messages should be logged in the file.
+        """
+        self.account.deposit(1000)
+        self.account.withdraw(500)
+        self.account.transfer(700, self.other_account)
+        log_messages = self._read_file_lines('test_log.txt')
+        len_log_messages = self._count_file_lines('test_log.txt')
+        self.assertEqual(len(log_messages), 5)
+        self.assertEqual(log_messages[0], 'Account created with balance: 2000\n')
+        self.assertEqual(log_messages[1], 'Deposited: 1000\n')
+        self.assertEqual(log_messages[2], 'Withdrew: 500\n')
+        self.assertEqual(log_messages[3], 'Withdrew: 700\n')
+        self.assertEqual(log_messages[4], f'Transferred: 700 to account: {self.other_account.get_account_number()}\n')
+        self.assertEqual(len_log_messages, 5)
+
+    @staticmethod
+    def _count_file_lines(file_name):
+        with open(file_name, 'r') as file:
+            content = file.readlines()
+            return len(content)
+
+    @staticmethod
+    def _read_file_lines(file_name):
+        with open(file_name, 'r') as file:
+            content = file.readlines()
+            return content
+
+    def tearDown(self):
+        """
+        This method is called after each test.
+        It is used to clean up the test environment.
+        """
+        # Remove the log files
+        if self.account.get_log_file():
+            if os.path.exists(self.account.get_log_file()):
+                os.remove(self.account.get_log_file())
--- unit_testing_python/bank_account.py
+++ unit_testing_python/bank_account.py
@@ -1,10 +1,30 @@
+import random
+
+
 class BankAccount:
-    def __init__(self, balance=0):
+    def __init__(self, balance=0, log_file=None):
+        self._account_number = random.randint(100000, 999999)
         self._balance = balance
+        self._log_file = log_file
+        self._log_transaction(f"Account created with balance: {balance}")
+
+    def _log_transaction(self, message):
+        if self._log_file:
+            with open(self._log_file, 'a') as file:
+                file.write(message + '\n')
+        else:
+            create_log = input("Do you want to create a log file? (yes/no): ")
+            if create_log.lower() == 'yes':
+                file_name = input("Enter the file name: ")
+                with open(file_name, 'a') as file:
+                    file.write(message + '\n')
+            else:
+                print("No log file created")
 
     def deposit(self, amount):
         if amount > 0:
             self._balance += amount
+            self._log_transaction(f"Deposited: {amount}")
             return self.get_balance()
         else:
             raise ValueError("Amount must be greater than 0")
@@ -12,6 +32,7 @@ class BankAccount:
     def withdraw(self, amount):
         if 0 < amount <= self._balance:
             self._balance -= amount
+            self._log_transaction(f"Withdrew: {amount}")
             return self.get_balance()
         else:
             raise ValueError("Amount must be greater than 0 and less than or equal to balance")
@@ -19,6 +40,7 @@ class BankAccount:
     def transfer(self, amount, account):
         if 0 < amount <= self._balance:
             self.withdraw(amount)
+            self._log_transaction(f"Transferred: {amount} to account: {account.get_account_number()}")
             account.deposit(amount)
             return self.get_balance()
         else:
@@ -27,3 +49,8 @@ class BankAccount:
     def get_balance(self):
         return self._balance
 
+    def get_account_number(self):
+        return self._account_number
+
+    def get_log_file(self):
+        return self._log_file
